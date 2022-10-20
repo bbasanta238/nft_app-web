1587604b55ba88c2a4cf8a9dd9fc77e1d95813cf
bbasanta238 <bbasanta238@gmail.com>
2022-10-20 09:34:50 +0545

    modifiying contract connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,6 @@ class ApplicationController < ActionController::Base
     
     def setGonData
         gon.web3StorageAPIKey= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDBFNTUyYmZEQWQ3Y2I0MjdkZDZlNTZkQzExNzg3N2M3NzY3ZGI5RTIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjYxNjkyMDI1MTYsIm5hbWUiOiJORlRfUHJvamVjdCJ9.QPYxmfbzubF1SPASlxj7LlazkQdBQPNs8oFNZmBwggs"
+        gon.contractAddress = "0xd51F21d8F873909e29203539B4D0d799dF2FDC33"
     end
 end
--- app/javascript/contractConnection.js
+++ app/javascript/contractConnection.js
@@ -1,10 +1,9 @@
 import ABI from "./abi/NFTContract.json";
 import { web3 } from "./custom/metamaskConnection";
-let lotteryContract;
+let NFTContract;
 
 async function contractConnection() {
-  // const deployedAddress = process.env.CONTRACT_ADDRESS
-  lotteryContract = new web3.eth.Contract(ABI, gon.contractAddress);
+  NFTContract = new web3.eth.Contract(ABI, gon.contractAddress);
 }
 
-export { contractConnection, lotteryContract };
+export { contractConnection, NFTContract };
